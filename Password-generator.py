0a643ad36aa817c715ebf12c316b310bdd128352
Rahul Ban <ban.rahul038@gmail.com>
2022-07-14 02:32:58 +0530

    Export passwords works

--- Password-generator.py
+++ Password-generator.py
@@ -4,13 +4,19 @@
 
 import hashlib
 import random
-
+import pandas as pd
 
 def generate_password(password, num):
+    pwd_list = []
     for i in range(1, num):
         pwd_gen = hashlib.sha256(str(password+str(i)).encode()).hexdigest()
         cut_pwd = random.choice(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')) + random.choice(list('.,!@#$%^&*()_+=-')) + pwd_gen[:8] + random.choice(list('ABCDEFGHIJKLMNOPQRSTUVWXYZ')) + pwd_gen[9:14]
+        pwd_list.append(cut_pwd)
         print("Secure Password ", i, " : ", cut_pwd)
+    dict = {"Secure Passwords" : pwd_list}
+    df = pd.DataFrame(dict)
+    df.to_csv('Secure_passwords.csv')
+    print("Secure_passwords.csv : exported successfully !")
 
 def generate_more(choice):
     if (choice == "y" or choice == "Y"):
@@ -25,7 +31,7 @@ def main():
         try:
             num = int(input("Enter how many passwords to generate: "))
             generate_password(password, num+1)
-            generate_more(input("Generate more passwords? (y) \n"))
+            generate_more(input("Generate more passwords? (y/n) \n"))
             FALSE
         except ValueError:
             print("Please enter a valid number.")
